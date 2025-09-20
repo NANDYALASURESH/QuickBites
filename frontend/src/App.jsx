7527c9257eb0e030d22dffc89a8b28c10df22eb0
Suresh <nandyalasuresh143@gmail.com>
2025-09-20 09:57:55 +0530

    Add frontend files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import "../src/app.css"
-import QuickBiteWelcome from "./pages/welcome"
-import Login from '../src/pages/login';
-import HomePage from './pages/UserDashboard';
-import ProtectedRoute from './ProtectedRoute/app';
-import QuickBiteCart from './pages/cart';
-import PublicRoute from './PublicRoute/app';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<PublicRoute><QuickBiteWelcome/></PublicRoute>} />
-        <Route path='/cart' element={<ProtectedRoute><QuickBiteCart /></ProtectedRoute>} />
-        <Route path='/login' element={<PublicRoute><Login /></PublicRoute>} />
-        <Route path='/home' element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
-        
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
